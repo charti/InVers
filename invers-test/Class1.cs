7d99931bdff2bc827c956b6ad9765011a2765bb2
charti <chr.chart@gmail.com>
2015-09-13 16:06:06 +0200

    Nunit Dummy

---
+++ invers-test/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invers_test
+{
+    using NUnit.Framework;
+    public class DummyTest
+    {
+        [Test]
+        void dummy_1_success()
+        {
+            Assert.That(true != false);
+        }
+
+        [Test]
+        void dummy_2_fail()
+        {
+            Assert.That(false == true);
+        }
+    }
+}
